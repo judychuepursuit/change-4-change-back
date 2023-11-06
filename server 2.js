370891b736fff9e932125f268f9b94cc3175251a
Sabri Mohiuddin <you@example.com>
2023-11-06 03:05:08 -0500

    going to push in to payment_SB

---
+++ server 2.js
@@ -0,0 +1,12 @@
+//   // DEPENDENCIES
+//   const app = require("./app.js");
+
+//   // CONFIGURATION
+//   require("dotenv").config();
+//   const PORT = process.env.PORT;
+  
+//   // LISTEN
+//   app.listen(PORT, () => {
+//     console.log(`change-4-change App is listening on port ${PORT}`);
+//   });
+  
