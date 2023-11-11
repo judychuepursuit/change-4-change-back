6ef322f419ec39482a5a4fe75e2f38fc0bfac3aa
Sabri Mohiuddin <you@example.com>
2023-11-11 16:50:23 -0500

    new push

--- server.js
+++ server.js
@@ -268,37 +268,113 @@ app.post('/payment', async (req, res) => {
     }
 });
 
+// app.post('/create-payment-link', async (req, res) => {
+//     try {
+//         const { productName, amount } = req.body;
+
+//         if (!productName || !amount) {
+//             return res.status(400).json({ error: 'Product name and amount are required.' });
+//         }
+
+//         // Convert amount to the smallest currency unit, e.g., cents
+//         const productAmount = Math.round(parseFloat(amount) * 100);
+
+//         // You would typically have product and price creation in an admin interface, not in the payment flow
+//         const product = await stripe.products.create({
+//             name: productName,
+//         });
+
+//         const price = await stripe.prices.create({
+//             unit_amount: productAmount,
+//             currency: 'usd',
+//             product: product.id,
+//         });
+
+//         const paymentLink = await stripe.paymentLinks.create({
+//             line_items: [{ price: price.id, quantity: 1 }],
+//         });
+
+//         res.json({ url: paymentLink.url });
+//     } catch (err) {
+//         console.error("Payment link error:", { error: err.message });
+//         res.status(500).json({ error: 'An error occurred while creating the payment link.' });
+//     }
+// });
+
+// app.post('/create-payment-link', async (req, res) => {
+//     try {
+//         const { productName, amount } = req.body;
+
+//         if (!productName || !amount) {
+//             return res.status(400).json({ error: 'Product name and amount are required.' });
+//         }
+
+//         // Ensure the amount is in cents and is a valid number
+//         const productAmount = Math.round(Number(amount) * 100);
+//         if (isNaN(productAmount)) {
+//             return res.status(400).json({ error: 'Invalid amount provided.' });
+//         }
+
+//         // Create a product or look up the product by name if already created
+//         let product = await stripe.products.list({ name: productName });
+//         if (!product.data.length) {
+//             product = await stripe.products.create({ name: productName });
+//         } else {
+//             product = product.data[0]; // Use the existing product
+//         }
+
+//         // Create a price for the product
+//         const price = await stripe.prices.create({
+//             unit_amount: productAmount,
+//             currency: 'usd',
+//             product: product.id,
+//         });
+
+//         // Create the payment link
+//         const paymentLink = await stripe.paymentLinks.create({
+//             line_items: [{ price: price.id, quantity: 1 }],
+//         });
+
+//         res.json({ url: paymentLink.url });
+//     } catch (err) {
+//         console.error("Payment link error:", err);
+//         res.status(500).json({ error: 'An error occurred while creating the payment link.' });
+//     }
+// });
+
 app.post('/create-payment-link', async (req, res) => {
-    try {
-        const { productName, amount } = req.body;
+  try {
+    const { productName, amount } = req.body;
 
-        if (!productName || !amount) {
-            return res.status(400).json({ error: 'Product name and amount are required.' });
-        }
-
-        // Convert amount to the smallest currency unit, e.g., cents
-        const productAmount = Math.round(parseFloat(amount) * 100);
-
-        // You would typically have product and price creation in an admin interface, not in the payment flow
-        const product = await stripe.products.create({
-            name: productName,
-        });
-
-        const price = await stripe.prices.create({
-            unit_amount: productAmount,
-            currency: 'usd',
-            product: product.id,
-        });
-
-        const paymentLink = await stripe.paymentLinks.create({
-            line_items: [{ price: price.id, quantity: 1 }],
-        });
-
-        res.json({ url: paymentLink.url });
-    } catch (err) {
-        console.error("Payment link error:", { error: err.message });
-        res.status(500).json({ error: 'An error occurred while creating the payment link.' });
+    if (!productName || !amount) {
+      return res.status(400).json({ error: 'Product name and amount are required.' });
     }
+
+    // Ensure amount is in cents
+    const productAmount = parseInt(amount) * 100;
+
+    const product = await stripe.products.create({
+      name: productName,
+    });
+
+    const price = await stripe.prices.create({
+      unit_amount: productAmount,
+      currency: 'usd',
+      product: product.id,
+    });
+
+    const paymentLink = await stripe.paymentLinks.create({
+      line_items: [{
+        price: price.id,
+        quantity: 1,
+      }],
+    });
+
+    res.json({ url: paymentLink.url });
+  } catch (err) {
+    console.error("Error during payment link creation:", err);
+    res.status(500).json({ error: 'An error occurred while creating the payment link.' });
+  }
 });
 
 // Handle unhandled promise rejections
