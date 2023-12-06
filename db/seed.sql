131bfe4c4f8f1c319f2867019a88a91d7f89e17c
Sabri Mohiuddin <you@example.com>
2023-12-05 20:46:54 -0500

    transactions

--- db/seed.sql
+++ db/seed.sql
@@ -6,4 +6,4 @@ INSERT INTO charities (name, stripe_account_id) VALUES
 ('UNICEF', 'acct_1OCtjmQS1DLaPBq0'); 
 
 INSERT INTO transactions (charity_id, amount, currency, donation_frequency, stripe_payment_intent_id, created_at) VALUES
-(0, 50, 'USD', 'one-time', 'pi_3OJp5qKw0bozpjFO0CIio5LR', '2023-01-01T00:00:00Z'),
+(1, 50, 'USD', 'one-time', 'pi_3OJp5qKw0bozpjFO0CIio5LR', '2023-01-01T00:00:00Z'),
