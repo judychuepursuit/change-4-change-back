1d0578c10e06acee46a73cb98ff01f2c4b72b76c
Allahvel Salisbury <allahvelsalisbury@pursuit.org>
2023-12-13 18:40:42 -0500

    Adjusted schema and seed files to take in registered users info as well. Users are now able register successfully throught the register page.

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -9,20 +9,23 @@ const register = async (req, res) => {
 
   try {
     const existingUser = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+    console.log("Hello !")
 
     if (existingUser.rows.length > 0) {
       return res.status(409).json({ message: 'Email is already registered' });
     }
-
+    console.log("user exists")
     const createdUser = await pool.query(
       'INSERT INTO users (first_name, last_name, birth_date, email, password) VALUES ($1, $2, $3, $4, $5) RETURNING *',
       [first_name, last_name, birth_date, email, password]
     );
+    console.log("should have created user")
     
 
     if (createdUser.rows.length > 0) {
       return res.status(201).json({ message: 'Registration successful' });
     }
+    console.log("registered")
 
   } catch (error) {
     console.error(error);
